a34f07e1a12024a968cfb97047088eda18193fb7
맥주보다소주연 <jjjjun0906@naver.com>
2024-01-24 16:03:40 +0900

    proxy setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/test")
+      .then((res) => res.text())
+      .then((m) => setMessage(m));
+  }, []);
   return (
     <div className="App">
+      {" "}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {" "}
+        <img src={logo} className="App-logo" alt="logo" /> <p> {message} </p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          {" "}
+          Learn React{" "}
+        </a>{" "}
+      </header>{" "}
     </div>
   );
 }
-
 export default App;
